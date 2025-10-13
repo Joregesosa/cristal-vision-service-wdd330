4b4fc024e4a68dde0041ba7af6bb0a5eb74974a7
Jorge Sosa <joregesosa@gmail.com>
2025-10-12 23:07:35 -0400

    Refactor click event listener in common.js for improved readability and structure

--- src/js/common.js
+++ src/js/common.js
@@ -1,3 +1,20 @@
-import {loadHeaderFooter} from './utils.mjs';
+import { loadHeaderFooter } from './utils.mjs';
 
 loadHeaderFooter();
+
+window.addEventListener('click', (event) => {
+    const link = event.target.closest('a');
+    
+    if (link) {
+        event.preventDefault();
+        let href = link.getAttribute('href');
+        
+        let destinationURL;
+        if (/^https?:\/\//.test(href)) {
+            destinationURL = href;
+        } else {
+            destinationURL = new URL(href, window.location.origin).href;
+        }
+ 
+    }
+}); 
